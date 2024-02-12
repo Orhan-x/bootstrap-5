dedd1dd897cf3652171ba7ea3b5a787cdcdb32d7
Orhan-x <oumoussacontact@gmail.com>
2024-02-12 23:24:27 +0100

    tried select in a form

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-let datalist = document.getElementById('exampleDataList')
-let datalistValues = document.querySelectorAll('#datalistOptions > option')
+// let datalist = document.getElementById('exampleDataList')
+// let datalistValues = document.querySelectorAll('#datalistOptions > option')
 
-let listOfValues = []
+// let listOfValues = []
 
-datalistValues.forEach(element => {
-    listOfValues.push(element.value)
-});
+// datalistValues.forEach(element => {
+//     listOfValues.push(element.value)
+// });
 
 
-datalist.onchange = function (event) {
-        if(listOfValues.includes(datalist.value)) {
-            console.log(datalist.value);
-        }else {
-            console.log("Doesn't exists!");
-        }
-}
+// datalist.onchange = function (event) {
+//         if(listOfValues.includes(datalist.value)) {
+//             console.log(datalist.value);
+//         }else {
+//             console.log("Doesn't exists!");
+//         }
+// }
+
+// try Seletct data extraction
+
+let selectInput = document.getElementById('cities')
+
+
+
+selectInput.addEventListener("change", function (e) {
+    console.log(selectInput.value);
+})
 
 
 
